e951c69bd1f99a6373e685c111091b01a94d99bb
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2023-02-13 15:48:42 +0530

    Add files via upload

---
+++ decom.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <zlib.h>
+
+const int CHUNK_SIZE = 1024;
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0] << " [input file] [output file]" << std::endl;
+    return 1;
+  }
+
+  // Open input and output files
+  std::ifstream input_file(argv[1], std::ios::binary);
+  std::ofstream output_file(argv[2], std::ios::binary);
+
+  // Initialize zlib structures
+  z_stream strm;
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  inflateInit(&strm);
+
+  // Buffer for the input and decompressed data
+  char input_data[CHUNK_SIZE];
+  char decompressed_data[CHUNK_SIZE];
+
+  // Decompress the input data chunk by chunk
+  do {
+    input_file.read(input_data, CHUNK_SIZE);
+    int bytes_read = input_file.gcount();
+    strm.avail_in = bytes_read;
+    strm.next_in = (Bytef *)input_data;
+    do {
+      strm.avail_out = CHUNK_SIZE;
+      strm.next_out = (Bytef *)decompressed_data;
+      inflate(&strm, Z_NO_FLUSH);
+      output_file.write(decompressed_data, CHUNK_SIZE - strm.avail_out);
+    } while (strm.avail_out == 0);
+  } while (input_file);
+
+  // Finish decompression
+  strm.avail_in = 0;
+  do {
+    strm.avail_out = CHUNK_SIZE;
+    strm.next_out = (Bytef *)decompressed_data;
+    inflate(&strm, Z_FINISH);
+    output_file.write(decompressed_data, CHUNK_SIZE - strm.avail_out);
+  } while (strm.avail_out == 0);
+
+  // Clean up and return
+  inflateEnd(&strm);
+  return 0;
+}
